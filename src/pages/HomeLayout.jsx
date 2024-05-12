9546b948b6dfecf654cb9044dc4defc800a8e055
Pratham Shah <itspratham2911@gmail.com>
2024-05-12 10:08:36 +0530

    added styling to landing page and completed it

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 const HomeLayout = () => {
 	return (
 		<div className=''>
-			<nav>This is an navbar</nav>
 			<Outlet />
 		</div>
 	);
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,36 @@
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
+
 const Landing = () => {
-	return <div>Landing</div>;
+	return (
+		<Wrapper>
+			<nav>
+				<Logo />
+			</nav>
+			<div className='container page'>
+				<div className='info'>
+					<h1>
+						Job <span>Tracking</span> App
+					</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Harum commodi dolores ab sed, provident molestias
+						consectetur odit sequi debitis veniam.
+					</p>
+
+					<Link to={"/register"} className='btn register-link'>
+						Register
+					</Link>
+					<Link to={"/register"} className='btn register-link'>
+						Login / Guest User
+					</Link>
+				</div>
+				<img src={main} alt='job hunt' className='img main-img' />
+			</div>
+		</Wrapper>
+	);
 };
+
 export default Landing;
